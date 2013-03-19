689ad3f0f0e0373c3bf29b45ecd805486d99c0e6
Bitmaker <Bitmaker@Bitmakers-iMac.local>
2013-03-19 16:52:52 -0400

    first commit, built up

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,51 @@
+class PicturesController < ApplicationController
+
+	before_filter :load_pictures
+
+	# def index
+	# 	@greeting = "Hello World"		
+	# end
+
+	def show
+		@picture = @pictures[params[:id].to_i]
+		
+	end
+
+	def load_pictures
+		@pictures = [
+		{:title => "First Love: Horses, Majestic Horses",
+		:artist => "Chief IronBone",
+		:url => 'http://pcdn.500px.net/28653251/6d6075fad5647dffd251e779c6b3a37c6a70f2ac/4.jpg'
+		},		
+		{:title => "Second Love: A Mouse, A Cute Little Mouse",
+		:artist => "Self-Portrait",
+		:url => 'http://pcdn.500px.net/28643261/c0e80a2ff2213205ba272b3f8ea010eafe6e1594/4.jpg'
+		},		
+
+		{:title => "Thursday Love: Laura",
+		:artist => "Picasso",
+		:url => 'http://pcdn.500px.net/28643491/8c0e5d2da96aff94680bddc3d589749e8be06b6f/4.jpg'
+		},		
+
+
+		{:title => "Friday Love: Maserati Girl",
+		:artist => "Maserati of Canada",
+		:url => 'http://pcdn.500px.net/9107825/aefa8eb17e4e77fc0f97dca91eda08a37a33d661/4.jpg'
+		},		
+
+
+		{:title => "Saturday Love:  SLS AMG - Wide Open for Bidness",
+		:artist => "Mercedes-Benzito",
+		:url => 'http://pcdn.500px.net/18648273/c001684f2397089830402a83e9b3260711f5a9e6/4.jpg'
+		},		
+
+		{:title => "Sunday Morning Drive Love:  Baby got back, and front, and Ferrari",
+		:artist => "Ferrari of Canada",
+		:url => 'http://pcdn.500px.net/1337978/34b90ecc16fcc4bd69c7adf7df844e8a5659d4cd/4.jpg'
+		}		
+		]
+	end
+
+end 
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photogrr::Application.config.secret_token = 'eaec71e7b55d285c7f3295a50d41030473d10bcd56784c9fd94b4f8fc1e2ca7eeb28c796cfdec620bee574eba7e366f9cadf661bb814c30088feaf56bcfcdc7b'
