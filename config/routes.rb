611345e75c7c820e0d0927ff5e6cdb40efd34b3b
AshBT <ashley.beattie@gmail.com>
2013-03-21 15:36:19 -0400

    this thing kinda works, Bootstrap CSS implemented

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,5 @@ Photogrr::Application.routes.draw do
 
   resources :pictures    #whenever you're talking about the model it is always singular
                       #whenever you're talking about the controller it is always plural
-                      
-  
-    
+ 
 end
